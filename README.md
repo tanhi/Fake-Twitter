7342fb59e00b7b7cc99c0c659454285aa055c4e3
tanhi <herrerart19@outlook.com>
2017-12-05 12:36:54 -0600

    ultima versión

---
+++ README.md
@@ -0,0 +1,38 @@
+## Twitter
+
+En este reto, vamos a replicar el newsfeed de Twitter, para ello vamos a seguir un flujo de versiones que te guiarán para enfocarte de una manera más eficiente :)
+
+Puedes adaptar el diseño como mejor te parezca, pero no tardes mucho definiendo los colores o fuentes. Por último puedes replicar el diseño de la página oficial :)
+
+## Objetivos
+
+Versión 0.0.1
+
+1. Diseñar un formulario que permita ingresar un texto y un botón para "twittear".
+2. Agregar un evento de click al botón o de submit al formulario.
+3. En el evento, obtener el texto.
+4. Agregar el texto al HTML.
+
+Versión 0.0.2
+
+1. No ingresar texto vacío (deshabilitar el botón de "twittear").
+2. Contar la cantidad de caracteres de forma regresiva.
+
+Versión 0.0.3
+
+1. Si pasa los 140 caracteres, deshabilitar el botón.
+2. Si pasa los 120 caracteres, mostrar el contador con OTRO color.
+3. Si pasa los 130 caracteres, mostrar el contador con OTRO color.
+4. Si pasa los 140 caracteres, mostrar el contador en negativo.
+
+Versión 0.0.4
+
+1. Al presionar enter(/n) que crezca el textarea de acuerdo al tamaño del texto.
+
+Versión 0.0.5 (Extra
+
+1. Si la cantidad de caracteres ingresados (sin dar un enter), supera al tamaño del textarea por defecto, debe de agregarse una línea más para que no aparezca el scroll. (Si en caso aplica)
+
+Versión 0.0.6 (Extra)
+
+1. Agregar la hora en que se publicó el tweet. En el formato de 24 horas: hh:mm.
--- js/app.js
+++ js/app.js
@@ -5,30 +5,19 @@ var button = document.getElementById("button-twittear");
 //evento para agregar contador de letras//
 var textArea = document.getElementById("words");
   textArea.addEventListener("keyup", ChangeColor); //teclado
+var size = document.getElementById("words");
+  size.addEventListener('keyup', autosize);
+  disableBtn();
 };
+//Para deshabilitir el botón de Twitt//
+function disableBtn() {
+    document.getElementById("button").disabled = true;
+  }
+
+function undisableBtn() {
+    document.getElementById("button").disabled = false;
+  }
 //creando funcion para ChangeColor//
-function ChangeColor (){
-  var maximum = "140";
-  var twitt = document.getElementById("words").value;
-  var long = twitt.length;
-  var character=document.getElementById("character");
-    //cambiar el color del contador si cambia la longitud del texto//
-    if(long>="0" & long<"120"){
-      character.style.color="black";
-      character.value = maximum-long;
-    }
-    else if(long>="120" & long<"130"){
-      character.style.color="yellow";
-      character.value = maximum-long;
-    }
-    else if(long>="130" & long<="140"){
-      character.style.color="red";
-      character.value = maximum-long;
-    }
-    else if(long>"140"){
-      character.value =("-"+(long-maximum));
-    }
-};
 
 //creando funcion para PostMessage//
 function PostMessage() {
@@ -51,6 +40,65 @@ function PostMessage() {
       }
   };
 
+  function ChangeColor (){
+    var maximum = "140";
+    var twitt = document.getElementById("words").value;
+    var long = twitt.length;
+    var character=document.getElementById("character");
+    //cambiar el color del contador si cambia la longitud del texto//
+    if(long>="0" & long<"120"){
+      character.style.color="black";
+      character.value = maximum-long;
+      return undisableBtn()
+    }
+    else if(long>="120" & long<"130"){
+      character.style.color="yellow";
+      character.value = maximum-long;
+      return undisableBtn()
+    }
+    else if(long>="130" & long<="140"){
+      character.style.color="red";
+      character.value = maximum-long;
+      return undisableBtn()
+    }
+    else if(long>"140"){
+      character.value =("-"+(long-maximum));
+      return disableBtn()
+    }
+  };
 
-//ubicando el focus en el  textArea//
-document.getElementById('words').focus();
+  function clock(){ //Función que da las hora del twitt//
+    var day = new Date();
+    hour = day.getHours();
+    minutes = day.getMinutes();
+    var dateTime = "AM";
+    if(hour === 12){
+        dateTime = "PM";
+    } else if(hour > 12){
+      dateTime = "PM";
+      hour = hour-12;
+    }
+    if (hour < 10) {
+        hour = '0' + hour;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    var clockSet = hour + ":" + minutes+ " " +dateTime;
+    return clockSet;
+  };
+
+  function autosize(){//función que oculta el scroll y autoajusta el textarea//
+      var el = this;
+      setTimeout(function(){
+        el.style.cssText = 'height:auto; padding:0';
+        // for box-sizing other than "content-box" use:
+        // el.style.cssText = '-moz-box-sizing:content-box';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+      },0);
+    };
+
+
+
+  //ubicando el focus en el  textArea//
+  document.getElementById('text').focus();
